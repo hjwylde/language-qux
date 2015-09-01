456ceceb30dd1fbe38a9af94c67544e95ba1d6db
Henry J. Wylde <public@hjwylde.com>
2015-09-01 23:48:09 +1200

    Updated version to use Data.Version and cabal's auto generated version constant

--- src/Language/Qux/Version.hs
+++ src/Language/Qux/Version.hs
@@ -1,34 +1,18 @@
 
 {-|
 Module      : Language.Qux.Version
-Description : Haskell constants of the language version.
+Description : Haskell constant of the language version.
 
 Copyright   : (c) Henry J. Wylde, 2015
 License     : BSD3
 Maintainer  : public@hjwylde.com
 
-Haskell constants of the language version.
+Haskell constant of the language version.
 -}
 
-module Language.Qux.Version where
+module Language.Qux.Version (
+    version
+) where
 
-import Data.List (intercalate)
-
-
--- * Version
-
--- | The version formatted as "major.minor.patch".
-version = intercalate "." (map show [major, minor, patch])
-
-
--- ** Components
-
--- | The major component.
-major = 0
-
--- | The minor component.
-minor = 1
-
--- | The patch component.
-patch = 0
+import Paths_language_qux (version)
 
