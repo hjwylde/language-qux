391b28b481111fb90d7fddc209c83def2918973b
Henry J. Wylde <public@hjwylde.com>
2015-10-12 21:17:14 +1300

    (#19) refactored exception and added in ResolveException

--- src/Language/Qux/Annotated/Exception.hs
+++ src/Language/Qux/Annotated/Exception.hs
@@ -11,15 +11,30 @@ Exceptions and utility functions.
 -}
 
 module Language.Qux.Annotated.Exception (
+    -- * Type class
+    CompilerException(..),
+
     -- * Type exception
     TypeException(..),
-    pos, message,
+
+    -- * Resolve exception
+    ResolveException(..),
 ) where
 
-import Data.List (intercalate)
+import Control.Exception
+
+import Data.List        (intercalate)
+import Data.Typeable
 
 import Language.Qux.Annotated.Parser (SourcePos)
 import Language.Qux.Syntax
+
+class CompilerException e where
+    -- | Extracts the source position from the exception.
+    pos :: e -> SourcePos
+
+    -- | Creates a human understandable message from the exception.
+    message :: e -> String
 
 
 -- | An exception that occurs during type checking. See "Language.Qux.Annotated.TypeChecker".
@@ -33,33 +48,48 @@ data TypeException  = TypeException SourcePos String        -- ^ A generic type 
                                                             --   arguments was passed to the
                                                             --   function call.
                     | MismatchedType SourcePos Id [Id]      -- ^ Indicates a type mismatch.
-    deriving Eq
+    deriving (Eq, Typeable)
+
+instance CompilerException TypeException where
+    pos (TypeException p _)             = p
+    pos (DuplicateFunctionName p _)     = p
+    pos (DuplicateParameterName p _)    = p
+    pos (InvalidFunctionCall p _ _)     = p
+    pos (MismatchedType p _ _)          = p
+
+    message (TypeException _ m)                         = m
+    message (DuplicateFunctionName _ name)              = "duplicate function name \"" ++ name ++ "\""
+    message (DuplicateParameterName _ name)             = "duplicate parameter name \"" ++ name ++ "\""
+    message (InvalidFunctionCall _ received expected)   = intercalate " " [
+        "invalid arguments count", show received,
+        "\nexpecting", show expected
+        ]
+    message (MismatchedType _ received expects)         = intercalate " " [
+        "unexpected type", "\"" ++ received ++ "\"",
+        "\nexpecting", sentence "or" expects
+        ]
+        where
+            sentence _ [x]  = x
+            sentence sep xs = intercalate " " [intercalate ", " (map show $ init xs), sep, show $ last xs]
+
+instance Exception TypeException
 
 instance Show TypeException where
     show e = show (pos e) ++ ":\n" ++ message e
 
--- | Extracts the source position from the exception.
-pos :: TypeException -> SourcePos
-pos (TypeException p _)             = p
-pos (DuplicateFunctionName p _)     = p
-pos (DuplicateParameterName p _)    = p
-pos (InvalidFunctionCall p _ _)     = p
-pos (MismatchedType p _ _)          = p
 
--- | Creates a human understandable message from the exception.
-message :: TypeException -> String
-message (TypeException _ m)                         = m
-message (DuplicateFunctionName _ name)              = "duplicate function name \"" ++ name ++ "\""
-message (DuplicateParameterName _ name)             = "duplicate parameter name \"" ++ name ++ "\""
-message (InvalidFunctionCall _ received expected)   = intercalate " " [
-    "invalid arguments count", show received,
-    "\nexpecting", show expected
-    ]
-message (MismatchedType _ received expects)         = intercalate " " [
-    "unexpected type", "\"" ++ received ++ "\"",
-    "\nexpecting", sentence "or" expects
-    ]
-    where
-        sentence _ [x]  = x
-        sentence sep xs = intercalate " " [intercalate ", " (map show $ init xs), sep, show $ last xs]
+-- | An exception that occurs during name resolution. See "Language.Qux.Annotated.NameResolver".
+data ResolveException   = ResolveException SourcePos String -- ^ A generic type exception with a
+                                                            --   position and message.
+    deriving (Eq, Typeable)
+
+instance CompilerException ResolveException where
+    pos (ResolveException p _) = p
+
+    message (ResolveException _ m) = m
+
+instance Exception ResolveException
+
+instance Show ResolveException where
+    show e = show (pos e) ++ ":\n" ++ message e
 
