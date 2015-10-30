7197e3e91b1d99cd6b8f11934b2dbcc5d9807bb3
Henry J. Wylde <public@hjwylde.com>
2015-10-30 17:03:19 +1300

    (#7) updated syntax in compiler for LLVM 3.5

--- src/Language/Qux/Llvm/Compiler.hs
+++ src/Language/Qux/Llvm/Compiler.hs
@@ -186,7 +186,7 @@ compileExpr (TypedExpr type_ (CallExpr id arguments))       = do
     n <- freeName
 
     append $ Name n := Call {
-        isTailCall              = False,
+        tailCallKind            = Nothing,
         Llvm.callingConvention  = C,
         Llvm.returnAttributes   = [],
         function                = Right $ ConstantOperand $ GlobalReference (compileType type_) (Name $ mangle id),
